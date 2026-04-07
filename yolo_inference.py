a3748aca2d7bc807cd7c452ee4d959b5c833d07c
jandorr <jandorr@hotmail.com>
2026-04-06 19:52:11 -0700

    Add court training notebook

--- yolo_inference.py
+++ yolo_inference.py
@@ -2,12 +2,17 @@ from ultralytics import YOLO
 from ultralytics import settings
 
 
-settings.update({"runs_dir": "/runs"})
+settings.update({"runs_dir": "runs"})
 
-model = YOLO("yolov8x")
-result = model.predict("input_videos/input_video.mp4", save=True)
+model = YOLO("models/yolov8x.pt")
+# model = YOLO("models/yolo5_last.pt")
+
+# result = model.predict("input_videos/image.png", save=True)
+# result = model.predict("input_videos/input_video.mp4", conf=0.2, save=True)
+result = model.track("input_videos/input_video.mp4", conf=0.2, save=True)
+
 
 print(result)
-print("boxes:")
-for box in result[0].boxes:
-    print(box)
+# print("boxes:")
+# for box in result[0].boxes:
+#     print(box)
